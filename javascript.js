f06bcc53825d9041a25593e6213c6449913a7db1
Sahsquatch <43761408+Sahsquatch@users.noreply.github.com>
2018-11-24 17:02:43 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+<script type="text/javascript"><!--
+var sounds = new Array();
+sounds[0]="music.mp3";
+sounds[1]="musci2.mp3";
+sounds[2]="music3.mp3"; 
+
+sounds[3]="music4.mp3";
+sounds[4]="music5.mp3";
+
+function getRandomSounds() {
+var randomNum = Math.floor(Math.random()*sounds.length);
+window.location = sounds[randomNum];
+}
+//--></script>
+
